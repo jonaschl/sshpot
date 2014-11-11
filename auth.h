8323f681bca6c85112cd7a6f537c90990cb73ad2
ZarkXe <zarkxe@gmail.com>
2014-11-11 13:41:30 +0100

    Juste re-indent

--- auth.h
+++ auth.h
@@ -6,14 +6,15 @@
 #define MAXBUF 100
 
 struct connection {
-    ssh_session session;
-    ssh_message message;
-    char client_ip[MAXBUF];
-    char con_time[MAXBUF];
-    char *user;
-    char *pass;
+	ssh_session session;
+	ssh_message message;
+	char client_ip[MAXBUF];
+	char con_time[MAXBUF];
+	char *user;
+	char *pass;
 };
 
 int handle_auth(ssh_session session);
 
 #endif
+
