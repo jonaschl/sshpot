9dcd707f0d65eadadaefe2272c8b4aff791b5802
ZarkXe <zarkxe@gmail.com>
2014-11-11 13:40:27 +0100

    Add max number of thread

--- config.h
+++ config.h
@@ -3,8 +3,9 @@
 
 #define LISTENADDRESS   "0.0.0.0"
 #define DEFAULTPORT     22
-#define RSA_KEYFILE     "./sshpot.rsa.key"
-#define LOGFILE         "sshpot_auth.log"
+#define RSA_KEYFILE     "/opt/sshpot/rsa-key/sshpot.rsa.key"
+#define LOGFILE         "/opt/sshpot/log-sshpot/sshpot_auth.log"
 #define DEBUG           0
+#define MAX_NB_THREAD   2048	
 
 #endif
